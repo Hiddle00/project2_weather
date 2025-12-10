0de7e272b3c216fd8e26f1d5f580f565d950eadd
YeonH <duswork01@gmail.com>
2025-12-10 16:25:53 +0900

    라벨링

--- project2/python_file/ML/base_temp.py
+++ project2/python_file/ML/base_temp.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, f1_score, classification_report
 
 # 1. 데이터 로드
-df = pd.read_csv("reviews_tokenized.csv")
+df = pd.read_csv("csv/reviews_tokenized.csv")
 #df_emotionset = df
 print(df.info())
 #print(df_emotionset.info())
@@ -21,5 +21,6 @@ clean_review = ~df['tokens'].astype(str).str.match(pattern)
 print(clean_review.sum())
 print(df[clean_review])
 
+print(df.info())
 #df = df['tokens']
 #df.to_csv('tokens_only.csv', index=False, encoding='utf-8-sig')
